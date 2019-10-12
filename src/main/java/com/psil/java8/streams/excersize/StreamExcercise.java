570e1baeedf5933f796aaf67d03c1941f6dd4f9c
Prabhat Kumar Sil <suprabhat.19@gmail.com>
2019-10-11 21:07:46 -0400

    java 8 practices and excercise

--- src/main/java/com/psil/java8/streams/excersize/StreamExcercise.java
+++ src/main/java/com/psil/java8/streams/excersize/StreamExcercise.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class StreamExcersize {
+public class StreamExcercise {
     public static void main(String[] args) {
 
         Trader raoul = new Trader("Raoul", "Cambridge");
